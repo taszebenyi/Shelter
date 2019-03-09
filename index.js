ad3a629dd2b1e64eab13db3c27741f43624a0d8d
taszebenyi <taszebenyi@gmail.com>
2019-03-09 13:48:03 +0100

    Add edit functionality

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('./config');
 require('./database');
 const errorHandler = require('./middlewares/errorHandler');
 const Animal = require('./models/animal');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+const methodOverride = require('method-override');
 const path = require('path');
 const express = require('express');
 
@@ -11,7 +14,11 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
+// INDEX ROUTE
 app.get('', (req, res, next) => {
   Animal.find({}, (error, animals) => {
     if(error) {
@@ -25,8 +32,7 @@ app.get('', (req, res, next) => {
   });
 });
 
-app.get('/animals/:id')
-
+// SHOW ROUTE
 app.get('/animals/:id', (req, res, next) => {
   Animal.findById(req.params.id, (error, animal) => {
     if(error) {
@@ -34,8 +40,62 @@ app.get('/animals/:id', (req, res, next) => {
       err.status = 404;
       next(err);
     }
-
     res.render('show', {animal});
+  });
+});
+
+// ADOPT ROUTE
+// MIGHT NOT BE TO EFFICIENT TO FETCH AGAIN FROM DB
+// app.get('/animals/:id/adopt', (req, res, next) => {
+//   Animal.findById(req.params.id, (error, animal) => {
+//     if(error) {
+//       let err = new Error('Animal by specified ID not found');
+//       err.status = 404;
+//       next(err);
+//     }
+//     res.render('adopt', {animal});
+//   });
+// });
+
+// SHOW EDIT ROUTE
+app.get('/animals/:id/edit', (req, res, next) => {
+  Animal.findById(req.params.id, (error, animal) => {
+    if(error) {
+      let err = new Error('Animal by specified ID not found');
+      err.status = 404;
+      next(err);
+    }
+    res.render('edit', {animal});
+  });
+});
+
+// EDIT ROUTE
+app.put('/animals/:id', (req, res, next) => {
+
+  // make sure to run validators
+  Animal.findOneAndUpdate( {_id: new ObjectID(req.params.id)}, req.body.animal, {useFindAndModify: false, new: true, runValidators: true}, (error, ani) => {
+
+    // error - to see validator err
+    // err - to see custom-made err
+    if(error) {
+      let err = new Error('Animal could not be edited');
+      err.status = 500;
+      next(error);
+    } else {
+      res.redirect('/animals/' + req.params.id);
+    }
+  });
+})
+
+// DELETE ROUTE
+app.delete('/animals/:id', (req, res, next) => {
+  Animal.findOneAndDelete({_id: new ObjectID(req.params.id)}, (error, animal) => {
+    if(error) {
+      let err = new Error('Animal could not be deleted');
+      err.status = 500;
+      next(err);
+    }
+    res.redirect('/');
   });
 });
 
