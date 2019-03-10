51bea2ac4e801836cea69c4e964f7d3ec7b1f28e
taszebenyi <taszebenyi@gmail.com>
2019-03-10 11:04:06 +0100

    heroku7

--- database.js
+++ database.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 const config = require('./config');
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
+console.log('***********************')
+console.log('***********************')
+console.log('***********************')
+console.log('***********************')
+console.log(process.env.MONGODB_URI)
+console.log('***********************')
+console.log('***********************')
+console.log('***********************')
+console.log('***********************')
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 var db = mongoose.connection;
 
-// db.once("open", () => {
-//   console.log("Connection to database succeeded!");
-// });
+db.once("open", () => {
+  console.log("Connection to database succeeded!");
+});
