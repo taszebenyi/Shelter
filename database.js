5ec20992008c18fee381d402fbe93c18cb66e918
taszebenyi <taszebenyi@gmail.com>
2019-03-10 10:43:42 +0100

    heroku6

--- database.js
+++ database.js
@@ -5,6 +5,6 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 
-db.once("open", () => {
-  console.log("Connection to database succeeded!");
-});
+// db.once("open", () => {
+//   console.log("Connection to database succeeded!");
+// });
